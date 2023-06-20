bd071b91b0b0406f038fbb1e769841bfc20689ae
Iam Noone <iamn00necrypto@gmail.com>
2023-06-20 09:09:07 -0400

    upload 1

---
+++ src/consts/parameters.ts
@@ -0,0 +1,21 @@
+/** Change these values to configure the application for your own use. **/
+
+// Your smart contract address (available on the thirdweb dashboard)
+// For existing collections: import your existing contracts on the dashboard: https://thirdweb.com/dashboard
+export const contractConst = "0x48CD226bD3E3abb2Fb4f2746766f6f1f6Eb6b9f8";
+
+// The name of the chain your contract is deployed to.
+// Provide a string for standard chains, or import one from @thirdweb-dev/chains package.
+// Learn more: https://portal.thirdweb.com/react/react.thirdwebprovider#activechain-recommended
+export const chainConst = "polygon";
+
+// Configure the primary color for buttons and other UI elements
+export const primaryColorConst = "white";
+
+// Choose between "light" and "dark" mode
+export const themeConst = "dark";
+
+// Gasless relayer configuration options
+export const relayerUrlConst = ""; // OpenZeppelin relayer URL
+export const biconomyApiKeyConst = ""; // Biconomy API key
+export const biconomyApiIdConst = ""; // Biconomy API ID
